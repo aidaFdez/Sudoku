f9a7dcba0357d271172ab80f6eb306fbfcac9033
aidaFdez <33495958+aidaFdez@users.noreply.github.com>
2018-05-22 18:35:51 +0200

    Finished checkPossible
    Finished the function checkPossible and checked that it works properly with a couple of examples.

--- sudokuGen.py
+++ sudokuGen.py
@@ -12,30 +12,36 @@ def checkPossible (position, number, sudo):
 
     #Checks that number does not happen in the same column.
     while j < position:
-        if number == sudo(j):
+        if number == sudo[j]:
             return False
         j = j+9
     k = int(position/9)*9 #Sets the beginning of the row in k
     l = k+9 #Sets the ending of the row in l
 
     #Checks that the number does not happen in the row.
-    while k< l or k< position:
+    while (k< position):
         if number == sudo[k]:
             return False
         k = k+1
-    k = int(position/3)*3 #Sets the beginning of the square
+
+    k = int(position/27)*27 #Sets the beginning of the first square of the row of squares
+    k = k + (int(numB/3)*3) #Sets the beginning of the square
 
     #Checks the number does not happen in the same square.
-    while k< position:
+    j = 0
+    while ((k< position) and (j < 3)):
         b = k
         l = 0
         while l<3:
-            if number == sudo (b):
+            if number == sudo [b]:
                 return False
             b = b+1
+            l = l+1
         k = k+9
+        j = j+1
     return True
 
+#array.insert(index, value)
 def createSudo (visit, que):
-    if not (que[0][80] == Nothing):
-        return sudo
+    if (len (que[0]) == 81) and not (que[0][80]==0):
+        return que[0]
